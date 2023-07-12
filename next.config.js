2f8431e62c262a486cd88e5e004db309f4634ccc
dschoon <dschoon@gmail.com>
2023-07-11 23:14:42 -0500

    Initial tutorial repo

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://chat.openai.com',
-          },
-        ],
-      },
-    ];
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig;
